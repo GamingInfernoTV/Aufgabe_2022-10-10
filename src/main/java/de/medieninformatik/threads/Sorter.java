f4163bb80f4c48a53355205c2ddb1bf275b6e292
Malte <maltekasolowsky@gmail.com>
2022-10-04 09:35:57 +0200

    Version 0.2: added merge range

--- src/main/java/de/medieninformatik/threads/Sorter.java
+++ src/main/java/de/medieninformatik/threads/Sorter.java
@@ -5,10 +5,18 @@ import java.util.concurrent.RecursiveTask;
 public class Sorter<T> extends RecursiveTask<T[]> {
     private final int SWITCH_SIZE;
     private final T[] values;
+    private final int left;
+    private final int right;
 
     public Sorter(int switch_size, T[] values) {
+        this(switch_size, values, 0, values.length); ;
+    }
+
+    private Sorter(int switch_size, T[] values, int left, int right) {
         SWITCH_SIZE = switch_size;
         this.values = values;
+        this.left = left;
+        this.right = right;
     }
 
     @Override
@@ -16,7 +24,7 @@ public class Sorter<T> extends RecursiveTask<T[]> {
         return null;
     }
 
-    private void mergeSort(T[] values) {
+    private void mergeSort(T[] values, int left, int right) {
 
     }
 
