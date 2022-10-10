b2482b0d16abc7b7ecd575000641f6fecf508f01
malte <maltekasolowsky@gmail.com>
2022-10-10 12:25:12 +0200

    Version 0.2: prepared thread functionality

--- src/main/java/de/medieninformatik/main/Mainstream.java
+++ src/main/java/de/medieninformatik/main/Mainstream.java
@@ -8,16 +8,21 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Mainstream {
     public static void main(String[] args) {
-        final int size = 10_000;
-        final int switchSize = (int) (size * 0.1);
+        final int size = 10_000_000;
+        final int switchSize = 100;
         final int nThreads = Runtime.getRuntime().availableProcessors();
-        final Integer[] values = (Integer[]) new Random().ints(size).boxed().toArray();
-
-        ForkJoinPool exec = new ForkJoinPool(nThreads);
+        final Integer[] values = new Random().ints(size).boxed().toArray(Integer[]::new);
+        final ForkJoinPool exec = new ForkJoinPool(nThreads);
         Integer[] compare = Arrays.copyOf(values, values.length);
-        Integer[] result = exec.invoke(new Sorter<>(switchSize, values));
+        long start = System.currentTimeMillis();
+        exec.invoke(new Sorter<>(switchSize, values, Integer::compareTo));
+        long end = System.currentTimeMillis() - start;
+        System.out.println("Sorting needed: " + end);
+        start = System.currentTimeMillis();
         Arrays.parallelSort(compare);
-        boolean success = Arrays.compare(result, compare) == 0;
-        System.out.printf("Sorting was%s successful", success ? " " : " not");
+        end = System.currentTimeMillis() - start;
+        System.out.println("Compare Sorting needed: " + end);
+        boolean success = Arrays.compare(values, compare) == 0;
+        System.out.printf("Sorting was%s successful", success ? "" : " not");
     }
 }
--- src/main/java/de/medieninformatik/threads/Sorter.java
+++ src/main/java/de/medieninformatik/threads/Sorter.java
@@ -1,34 +1,40 @@
 package de.medieninformatik.threads;
 
-import java.util.concurrent.RecursiveTask;
+import java.util.Comparator;
+import java.util.concurrent.RecursiveAction;
 
-public class Sorter<T> extends RecursiveTask<T[]> {
+public class Sorter<T> extends RecursiveAction {
     private final int SWITCH_SIZE;
     private final T[] values;
     private final int left;
     private final int right;
+    private final Comparator<T> comparator;
 
-    public Sorter(int switch_size, T[] values) {
-        this(switch_size, values, 0, values.length); ;
+    public Sorter(int switch_size, T[] values, Comparator<T> c) {
+        this(switch_size, values, 0, values.length, c);
     }
 
-    private Sorter(int switch_size, T[] values, int left, int right) {
+    private Sorter(int switch_size, T[] values, int left, int right, Comparator<T> c) {
         SWITCH_SIZE = switch_size;
         this.values = values;
         this.left = left;
         this.right = right;
+        this.comparator = c;
     }
 
     @Override
-    protected T[] compute() {
-        return null;
-    }
-
-    private void mergeSort(T[] values, int left, int right) {
-
-    }
-
-    private void insertionSort(T[] values) {
-
+    protected void compute() {
+        if (left < right + SWITCH_SIZE) {
+            // TODO: insertion sort
+        }
+        else {
+            final int mid = (left + right) / 2;
+            final Sorter<T> leftSort = new Sorter<>(SWITCH_SIZE, values, left, mid, comparator);
+            final Sorter<T> rightSort = new Sorter<>(SWITCH_SIZE, values, mid, right, comparator);
+            rightSort.fork();
+            leftSort.compute();
+            rightSort.join();
+            // TODO: merge sorted arrays
+        }
     }
 }
