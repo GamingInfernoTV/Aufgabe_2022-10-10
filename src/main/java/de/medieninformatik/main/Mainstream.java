c4fe2227d4db1348a956484ff64769fbf394f018
malte <maltekasolowsky@gmail.com>
2022-10-11 13:40:53 +0200

    Version 0.8: cleanup and transferred insertion sort and merge algorithm to own method

--- src/main/java/de/medieninformatik/main/Mainstream.java
+++ src/main/java/de/medieninformatik/main/Mainstream.java
@@ -16,14 +16,14 @@ public class Mainstream {
         final Integer[] compare = Arrays.copyOf(values, values.length);
         final ForkJoinPool exec = new ForkJoinPool(nThreads);
 
-        System.out.println("Sorting array using own implementation");
+        System.out.println("Sorting array using own implementation...");
         sortArray(values, arr -> exec.invoke(new Sorter<>(switchSize, arr, Integer::compareTo)));
 
-        System.out.println("Sorting array using build in sort");
+        System.out.println("Sorting array using build in sort...");
         sortArray(compare, Arrays::sort);
 
         boolean success = Arrays.equals(values, compare);
-        System.out.printf("Sorting was%s successful%n", success ? "" : " not");
+        System.out.printf("Sorting was%s successful.%n", success ? "" : " not");
         if (!success) {
             System.out.println(Arrays.toString(values));
             System.out.println(Arrays.toString(compare));
@@ -33,6 +33,6 @@ public class Mainstream {
     private static <T> void sortArray(T[] values, Consumer<T[]> sorter) {
         long start = System.currentTimeMillis();
         sorter.accept(values);
-        System.out.printf("Sorting needed %dms%n", System.currentTimeMillis() - start);
+        System.out.printf("Sorting needed: %dms%n", System.currentTimeMillis() - start);
     }
 }
--- src/main/java/de/medieninformatik/threads/Sorter.java
+++ src/main/java/de/medieninformatik/threads/Sorter.java
@@ -5,18 +5,18 @@ import java.util.Comparator;
 import java.util.concurrent.RecursiveAction;
 
 public class Sorter<T> extends RecursiveAction {
-    private final int SWITCH_SIZE;
+    private final int switchSize;
     private final T[] values;
     private final int left;
     private final int right;
     private final Comparator<T> comparator;
 
-    public Sorter(int switch_size, T[] values, Comparator<T> c) {
-        this(switch_size, values, 0, values.length, c);
+    public Sorter(int switchSize, T[] values, Comparator<T> c) {
+        this(switchSize, values, 0, values.length, c);
     }
 
-    private Sorter(int switch_size, T[] values, int left, int right, Comparator<T> c) {
-        SWITCH_SIZE = switch_size;
+    private Sorter(int switchSize, T[] values, int left, int right, Comparator<T> c) {
+        this.switchSize = switchSize;
         this.values = values;
         this.left = left;
         this.right = right;
@@ -25,44 +25,48 @@ public class Sorter<T> extends RecursiveAction {
 
     @Override
     protected void compute() {
-        if (right - left <= SWITCH_SIZE) {
-            // TODO: implement insertion sort from left to right (exclusive)
-            for (int i = left; i < right; i++) {
-                T valuesToSort = values[i];
-                int j = i;
-                while (j > left && comparator.compare(valuesToSort, values[j - 1]) < 0) {
-                    values[j] = values[j - 1];
-                    j--;
-                }
-                values[j] = valuesToSort; //Das kannst du dann löschen
-            }
+        if (right - left <= switchSize) {
+            insertionSort(values, left, right, comparator);
         } else {
             final int mid = (left + right) / 2;
-            final Sorter<T> leftSort = new Sorter<>(SWITCH_SIZE, values, left, mid, comparator);
-            final Sorter<T> rightSort = new Sorter<>(SWITCH_SIZE, values, mid, right, comparator);
+            final Sorter<T> leftSort = new Sorter<>(switchSize, values, left, mid, comparator);
+            final Sorter<T> rightSort = new Sorter<>(switchSize, values, mid, right, comparator);
+
             rightSort.fork();
             leftSort.compute();
             rightSort.join();
-            int leftSize = mid - left;
-            int rightSize = right - mid;
 
-            T[] leftArr = Arrays.copyOfRange(this.values, left, mid);
-            T[] rightArr = Arrays.copyOfRange(this.values, mid, right);
+            merge(values, left, mid, right, comparator);
+        }
+    }
 
-            int i = 0, j = 0, k = left;
-            while (i < leftSize && j < rightSize) {
-                if (comparator.compare(leftArr[i], rightArr[j]) <= 0) {
-                    values[k++] = leftArr[i++];
-                } else {
-                    values[k++] = rightArr[j++];
-                }
+    private static <T> void insertionSort(T[] values, int left, int right, Comparator<T> c) {
+        for (int i = left; i < right; i++) {
+            T valuesToSort = values[i];
+            int j = i;
+            while (j > left && c.compare(valuesToSort, values[j - 1]) < 0) {
+                values[j] = values[j - 1];
+                j--;
             }
-            while (i < leftSize) {
-                values[k++] = leftArr[i++];
-            }
-            while (j < rightSize) {
-                values[k++] = rightArr[j++];
-            }
+            values[j] = valuesToSort;
+        }
+    }
+
+    private static <T> void merge(T[] values, int left, int mid, int right, Comparator<T> c) {
+        final int leftSize = mid - left;
+        final int rightSize = right - mid;
+        final T[] leftArr = Arrays.copyOfRange(values, left, mid);
+        final T[] rightArr = Arrays.copyOfRange(values, mid, right);
+
+        int i = 0, j = 0, k = left;
+        while (i < leftSize && j < rightSize) {
+            values[k++] = c.compare(leftArr[i], rightArr[j]) <= 0 ? leftArr[i++] : rightArr[j++];
+        }
+        while (i < leftSize) {
+            values[k++] = leftArr[i++];
+        }
+        while (j < rightSize) {
+            values[k++] = rightArr[j++];
         }
     }
 }
